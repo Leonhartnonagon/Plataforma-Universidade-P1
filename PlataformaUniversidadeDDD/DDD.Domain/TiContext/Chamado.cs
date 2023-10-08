a0facceb7aed6a50484e40cf851ccfd88050c995
Leonardo Barreto <leonardo@adven.tech>
2023-10-08 15:54:28 -0300

    Criação das classes de bounded context no domain

---
+++ PlataformaUniversidadeDDD/DDD.Domain/TiContext/Chamado.cs
@@ -0,0 +1,19 @@
+﻿using DDD.Domain.SecretariaContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.TiContext
+{
+    public class Chamado
+    {
+        public int IdChamado { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public bool Resolvido { get; set; }
+        public List<Chamador> Chamador { get; set; }
+        public List<Tecnico>? Tecnico { get; set; }
+    }
+}
